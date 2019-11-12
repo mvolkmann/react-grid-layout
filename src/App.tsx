8b123196608d423b427a33f6a1786d85571a7beb
R. Mark Volkmann <mark@ociweb.com>
2019-11-12 13:06:53 -0600

    changed to work in IE11

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import GridLayout from './grid-layout';
 import Slot from './slot';
 import './App.css';
 
 function App() {
+  const myStyle = {
+    display: '-ms-grid',
+    msGridRows: '200px 200px',
+    msGridColumns: '200px 200px',
+  };
+
+  const getGrid = (row: number, column: number) => ({
+    msGridRow: row, // TypeScript doesn't recognize this CSS property
+    msGridColumn: column // TypeScript doesn't recognize this CSS property
+  });
+
+  const foo: CSSProperties = {};
+
   return (
-    <GridLayout
-      gridTemplateColumns="300px 1fr 1fr 200px"
-      gridTemplateRows="200px 200px"
-      gridTemplateAreas={[
-        'travel marketing marketing image',
-        'travel content benefits image'
-      ]}
-    >
-      <Slot name="benefits">Benefits</Slot>
-      <Slot name="content">Content</Slot>
-      <Slot name="image">Image</Slot>
-      <Slot name="marketing">Marketing</Slot>
-      <Slot name="travel">Travel</Slot>
-    </GridLayout>
+    <div>
+      <h1>React Grid Layout</h1>
+
+      <div className="my-grid">
+        <div className="a">A</div>
+        <div className="b">B</div>
+        <div className="c">C</div>
+        <div className="d">D</div>
+      </div>
+
+      <div style={myStyle}>
+        {/*
+        // @ts-ignore */}
+        <div className="a2" style={getGrid(2, 2)}>A</div>
+        {/*
+        // @ts-ignore */}
+        <div className="b2" style={getGrid(2, 1)}>B</div>
+        {/*
+        // @ts-ignore */}
+        <div className="c2" style={getGrid(1, 1)}>C</div>
+        {/*
+        // @ts-ignore */}
+        <div className="d2" style={getGrid(1, 2)}>D</div>
+      </div>
+
+      <GridLayout
+        gridTemplateColumns="300px 1fr 1fr 200px"
+        gridTemplateRows="200px 200px"
+        gridTemplateAreas={[
+          'travel marketing marketing image',
+          'travel content benefits image'
+        ]}
+      >
+        <Slot name="benefits">Benefits</Slot>
+        <Slot name="content">Content</Slot>
+        <Slot name="image">Image</Slot>
+        <Slot name="marketing">Marketing</Slot>
+        <Slot name="travel">Travel</Slot>
+      </GridLayout>
+    </div>
   );
 }
 
--- src/grid-layout.tsx
+++ src/grid-layout.tsx
@@ -2,8 +2,11 @@ import React, {ReactElement, useEffect, useRef} from 'react';
 import {StyleMap} from './component-types';
 import './grid-layout.scss';
 
+const {userAgent} = window.navigator;
+const isIE = userAgent.includes('MSIE') || userAgent.includes('Trident');
+
 type Props = {
-  children?: ReactElement[];
+  children: ReactElement[];
   gridTemplateAreas: string[];
   gridTemplateColumns: string;
   gridTemplateRows: string;
@@ -28,12 +31,60 @@ export default function GridLayout({
         'grid-template-areas',
         gridTemplateAreas.map(gta => `'${gta}'`).join(' ')
       );
+      if (isIE) ieGridFix();
     }
   }, [gridTemplateAreas, gridTemplateColumns, gridTemplateRows]);
+
+  // Adds CSS with vendor prefixes for IE11 grid support.
+  function ieGridFix() {
+    const {current} = myRef;
+    if (!current) return;
+
+    current.style.setProperty('-ms-grid-columns', gridTemplateColumns);
+    current.style.setProperty('-ms-grid-rows', gridTemplateRows);
+
+    const templateAreas: string[][] = [];
+    gridTemplateAreas.forEach((gta, rowIndex) => {
+      const names = gta.split(' ');
+      names.forEach((name, columnIndex) => {
+        let row = templateAreas[rowIndex];
+        if (!row) row = templateAreas[rowIndex] = [];
+        row[columnIndex] = name;
+      });
+    });
+
+    Array.from(current.children).forEach((child, index) => {
+      const slotName = child.id;
+      const rowIndex = templateAreas.findIndex(row => row.includes(slotName));
+      if (rowIndex !== -1) {
+        // @ts-ignore
+        child.style.setProperty('-ms-grid-row', rowIndex + 1);
+
+        const row = templateAreas[rowIndex];
+        const columnIndex = row.findIndex(name => name === slotName);
+        // @ts-ignore
+        child.style.setProperty('-ms-grid-column', columnIndex + 1);
+
+        let columnSpan = 1;
+        for (let c = columnIndex + 1; c < row.length; c++) {
+          if (row[c] === slotName) columnSpan++;
+        }
+        // @ts-ignore
+        if (columnSpan > 1) child.style.setProperty('-ms-grid-column-span', columnSpan);
+
+        let rowSpan = 1;
+        for (let r = rowIndex + 1; r < templateAreas.length; r++) {
+          if (templateAreas[r][columnIndex] === slotName) rowSpan++;
+        }
+        // @ts-ignore
+        if (rowSpan > 1) child.style.setProperty('-ms-grid-row-span', rowSpan);
+      }
+    });
+  }
  
   function getSlotContent(slotName: string): ReactElement | undefined {
     if (!children) return undefined;
-    return Array.from(children).find(child => child.props.name === slotName);
+    return children.find(child => child.props.name === slotName);
   }
  
   const gridAreas = new Set<string>();
@@ -45,7 +96,7 @@ export default function GridLayout({
   return (
     <div className="grid-layout" ref={myRef} style={style}>
       {Array.from(gridAreas).map(gridArea => (
-        <div key={gridArea} style={{gridArea}}>
+        <div id={gridArea} key={gridArea} style={{gridArea}}>
           {getSlotContent(gridArea)}
         </div>
       ))}
